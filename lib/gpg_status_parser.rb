9a68a7f8123483e3c81a1384468466aa3d22f21e
Grant Olson <kgo@grant-olson.net>
2013-02-15 20:11:47 -0500

    GPGStatusParser.parse iterates through file

--- lib/gpg_status_parser.rb
+++ lib/gpg_status_parser.rb
@@ -2,8 +2,19 @@ require 'gpg_status_parser/status_message'
 
 module GPGStatusParser
 
+  # Parse a single line
   def self.parse_line(line)
-    GPGStatusParser::StatusMessage.new(line)
+    GPGStatusParser::StatusMessage.new(line.strip)
+  end
+
+  # Takes a file and optional block and either yields or returns a
+  # list of status messages
+  def self.parse(file, &block)
+    if block
+      file.each_line { |line| yield parse_line(line)}
+    else
+      file.each_line.map{ |line| parse_line(line)}
+    end
   end
 
 end
--- test/test_main_api.rb
+++ test/test_main_api.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'stringio'
 
 class MainApiTest < Test::Unit::TestCase
   def test_parse_line
@@ -27,5 +28,36 @@ class MainApiTest < Test::Unit::TestCase
     end
   end
   
+  def test_parse_no_block
+    status_text = <<STATUS
+[GNUPG:] PLAINTEXT 74 0 
+[GNUPG:] SIG_ID jRWzDyjdbbGjvn9nNJJxF7HA7MA 2013-02-10 1360499004
+[GNUPG:] GOODSIG 703B0C0E25D38721 Test User <test@example.org>
+[GNUPG:] VALIDSIG B2AE14D84135BA08C0FC27C7703B0C0E25D38721 2013-02-10 1360499004 0 4 0 1 2 01 B2AE14D84135BA08C0FC27C7703B0C0E25D38721
+[GNUPG:] TRUST_ULTIMATE
+STATUS
+
+    status_file = StringIO.new(status_text)
+    res = GPGStatusParser.parse(status_file).map{|msg| msg.status}
+    good_res = [:PLAINTEXT, :SIG_ID, :GOODSIG, :VALIDSIG, :TRUST_ULTIMATE]
+    assert (res == good_res), "Results didn't match expected"
+  end
+  
+  def test_parse_block
+    status_text = <<STATUS
+[GNUPG:] PLAINTEXT 74 0 
+[GNUPG:] SIG_ID jRWzDyjdbbGjvn9nNJJxF7HA7MA 2013-02-10 1360499004
+[GNUPG:] GOODSIG 703B0C0E25D38721 Test User <test@example.org>
+[GNUPG:] VALIDSIG B2AE14D84135BA08C0FC27C7703B0C0E25D38721 2013-02-10 1360499004 0 4 0 1 2 01 B2AE14D84135BA08C0FC27C7703B0C0E25D38721
+[GNUPG:] TRUST_ULTIMATE
+STATUS
+    good_res = [:PLAINTEXT, :SIG_ID, :GOODSIG, :VALIDSIG, :TRUST_ULTIMATE].reverse
+
+    status_file = StringIO.new(status_text)
+    res = GPGStatusParser.parse(status_file) do |msg|
+      assert (msg.status == good_res.pop), "Didn't match expected status"
+    end
+    
+  end
   
 end
